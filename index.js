360d7ea20326e59a23342fe78355e3d7276e30b7
 <Mac@s-MacBook-Pro.local>
2019-07-22 21:16:24 -0400

    server running on port 5000

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Server Running On Port ${port} **\n`));
